1fbf2b533e2fed25664a755c89d2729123d1275e
Maycol Pereyra <maycolpereyrainoa@gmail.com>
2024-08-17 00:42:00 -0400

    FEAT:

--- ProyectoIntegrador.api/ProyectoIntegrador/obj/Debug/net8.0/ProyectoIntegrador.AssemblyInfo.cs
+++ ProyectoIntegrador.api/ProyectoIntegrador/obj/Debug/net8.0/ProyectoIntegrador.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProyectoIntegrador")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1f630767a05e556b7a8d0a9dd0a684bb1ea5b35")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1e445038c18442eca419c0ae3885e33aef4c485")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProyectoIntegrador")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProyectoIntegrador")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
